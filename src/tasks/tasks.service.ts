443ae9671c4a681b889f024e8e19d9e3597a090c
Chanakan Nuekreo <chanakarn@primo.mobi>
2020-08-09 23:01:18 +0700

    [FEATURE] Creating a Task: Service

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,10 +1,24 @@
 import { Injectable } from '@nestjs/common';
 
-import { Task } from './task.model';
+import { v4 as uuidv4 } from 'uuid';
+
+import { Task, TaskStatus } from './task.model';
 
 @Injectable()
 export class TasksService {
   private tasks: Task[] = [];
+
+  createTask(attributes: { title: string; description: string }) {
+    const task: Task = {
+      id: uuidv4(),
+      status: TaskStatus.OPEN,
+      ...attributes,
+    };
+
+    this.tasks.push(task);
+
+    return task;
+  }
 
   getAllTasks() {
     return this.tasks;
